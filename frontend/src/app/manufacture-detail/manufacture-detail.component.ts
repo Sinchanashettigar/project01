7f3d951e8686ac96c06f849cb0be5be8a9c0d70c
sinchana <sinchanashettygar@gmail.com>
2025-03-24 17:08:28 +0530

    initial commit

---
+++ frontend/src/app/manufacture-detail/manufacture-detail.component.ts
@@ -0,0 +1,15 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-manufacture-detail',
+//   templateUrl: './manufacture-detail.component.html',
+//   styleUrls: ['./manufacture-detail.component.css']
+// })
+// export class ManufactureDetailComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
